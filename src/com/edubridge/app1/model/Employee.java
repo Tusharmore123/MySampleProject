7259486d718942d4c38081deecd1ab353f99df22
Tusharmore123 <tusharmore160102@gmail.com>
2023-10-18 17:49:17 +0530

    My First commit

---
+++ src/com/edubridge/app1/model/Employee.java
@@ -0,0 +1,36 @@
+package com.edubridge.app1.model;
+
+public class Employee {
+	String employeeName;
+	int employeeId;
+	float employeeSalary;
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	@Override
+	public String toString() {
+		return "Employee [employeeName=" + employeeName + ", employeeId=" + employeeId + ", employeeSalary="
+				+ employeeSalary + "]";
+	}
+	public Employee(String employeeName, int employeeId, float employeeSalary) {
+		super();
+		this.employeeName = employeeName;
+		this.employeeId = employeeId;
+		this.employeeSalary = employeeSalary;
+	}
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public float getEmployeeSalary() {
+		return employeeSalary;
+	}
+	public void setEmployeeSalary(float employeeSalary) {
+		this.employeeSalary = employeeSalary;
+	}
+}
